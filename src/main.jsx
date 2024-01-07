0723d553e2353e29794614b6a10eed167a165fc6
Ruchir Jain <114685271+ruchirjainn@users.noreply.github.com>
2024-01-08 00:43:32 +0530

    updating store.js

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import './index.css';
 
 import { Provider } from 'react-redux';
-import store from '../../16_fast_react_pizza/src/store.js';
+import store from './store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
